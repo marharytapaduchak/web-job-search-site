92e1f1cb53766db82dce8572c6c418426284e024
Viktoriya Kibyeryeva <v.kibereva@gmail.com>
2026-04-01 10:44:55 +0300

    Changed the home page route to our page

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import UsefulMaterials from "./pages/UsefulMaterials";
 import ProfilePage from './pages/ProfilePage';
+import MainVacancies from './pages/MainVacancies';
 
-function HomePage() {
-  return <h1>Головна сторінка</h1>;
-}
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<MainVacancies />} />
         <Route path="/useful_materials" element={<UsefulMaterials />} />
         <Route path="/profile_page" element={<ProfilePage />} />
       </Routes>
